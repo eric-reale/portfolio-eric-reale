2b6053fd9135d87d74120a9f764b9c9c59e01614
Eric Reale <ecr5083@gmail.com>
2020-01-26 11:03:21 -0500

    buttons for project filters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-console.log('here');
+const buttons = document.querySelectorAll('button');
 
+function hover(e) {
+  e.currentTarget.classList.add('hover');
+}
 
+function removeHover(e) {
+  e.currentTarget.classList.remove('hover');
+}
+
+buttons.forEach(button => button.addEventListener('mouseover', hover))
+buttons.forEach(button => button.addEventListener('mouseout', removeHover))
